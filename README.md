e4173e076dc7cac7f79d167ba921eca601fce1ad
Ian Hakanson <gabianhak@gmail.com>
2024-10-05 01:27:16 -0300

    chore: init

---
+++ README.md
@@ -0,0 +1,5 @@
+### HOW TO USE
+The dataset goes in the assets directory
+The datasets that are in the testSets are necessary for the tests.py to run correctlyA
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+
+    path = "assets/testSets/modelo_muestra.csv"
+
+
+
+
+    print("hello, world")
+    file = open(path, "r")
+    print(file.read())
