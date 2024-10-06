73599e8b6ca24631e42d8b0fca9fea8778603cbc
Ian Hakanson <gabianhak@gmail.com>
2024-10-06 20:27:43 -0300

    chore: add venv to .gitignore

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 if __name__ == "__main__":
 
-    path = "assets/testDatasets/modelo_muestra.csv"
+    path = "assets/datasets/datos_nomivac_parte2.csv"
+    # path = "assets/testDatasets/modelo_muestra.csv"
     path2 = "output/broken_data.csv"
 
+    with open(path, "r") as file, open(path2, "w") as file2:
+        amount_masc_vacc = 0
+        amount_fem_vacc = 0
+        headers = file.readline().split(",")
+        map_of_set_of_values = [{x} for x in headers]
 
-    print("hello, world")
-    file = open(path, "r")
-    # print(file.readline())
-    fil2 = open(path2, "w")
-    fil2.write(file.readline())
+        for line in file:
+            line = line.rstrip().split(sep=",")
+            for i in range (0, len(headers)):
+                if headers[i] == "id_persona_dw\n":
+                    continue
+                if headers[i] == "sexo":
+                    amount_fem_vacc += line[i] == "F"
+                    amount_masc_vacc += line[i] == "M"
+
+                map_of_set_of_values[i].add(line[i])
+
+        for header in map_of_set_of_values:
+            print()
+            for item in header:
+                
+                print(item, end=", ")
+
+
+        print("amount of males: ", amount_masc_vacc)
+        print("amount of females: ", amount_fem_vacc)
+    
 
 
 
 
-
-    file.close()
