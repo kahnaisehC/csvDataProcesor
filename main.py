ec35444c371ab7aed9d726f0b12d668de5a7578a
Ian Hakanson <gabianhak@gmail.com>
2024-10-07 02:30:43 -0300

    Revert "feat: add csv reader"
    This reverts commit d441600bcf0a49f884b8b27ce22f02a57466133f.

--- main.py
+++ main.py
@@ -5,18 +5,13 @@ if __name__ == "__main__":
 
 
     print("hello, world")
-    with open(path, "r") as file:
-     with open(path2, "w") as broken_file_data:
-
-        # leer csv headers
-        headers_row = file.readline()
-        headers = headers_row.split(",")
-        for i in (0, len(headers)-1):
-            
-            headers[i] = headers[i].strip()
-    
-        # crear frequency map de los identificadores no unicos  
+    file = open(path, "r")
+    # print(file.readline())
+    fil2 = open(path2, "w")
+    fil2.write(file.readline())
 
 
 
-        # en el frequency map hacer regex para identificar los tipos de vacunas
+
+
+    file.close()
