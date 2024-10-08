431851ec1bde9e2dd643e002284a505f0c1cea2e
Ian Hakanson <gabianhak@gmail.com>
2024-10-08 04:09:04 -0300

    feat: add test files

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import string
 import json
+import re
 from os import SEEK_SET 
+
+
 # Path strings
 PATH_TO_INPUT_FORMAT_FILE="input/input_format.json"
 IGNORE_COLUMNS= {"id_persona_dw"}
-DATASET_PATH= "assets/testDatasets/modelo_muestra.csv"
-DATASET_PATH = "input/datos_nomivac_parte1.csv"
+DATASET_PATH= "assets/testDatasets/regex_test_dataset.csv"
+# DATASET_PATH = "input/datos_nomivac_parte1.csv"
 BROKEN_DATA_PATH = "output/broken_data.csv"
 
 # misc constants
@@ -16,11 +19,11 @@ def check_state_of_fields(fields:list[str], headers:list[str], frequency_map:set
     # possible_state_of_fields
     # El profe dijo que no podiamos tener loosey goosey strings so... ¯\_(ツ)_/¯
     CLEAR = "Clear"
-    SIZE_ERROR = "Size Mismatch between fields and headers"
-    LIST_ERROR = "Value wasn't find in list"
-    LISTS_ERROR = "Value wasn't find in lists"
-    REGEX_ERROR = "Value didn't match regex in input"
-    RANGE_ERROR = "Value out of range"
+    SIZE_ERROR = "Size Mismatch between fields and headers; "
+    LIST_ERROR = "Value wasn't find in list; "
+    LISTS_ERROR = "Value wasn't find in lists; "
+    REGEX_ERROR = "Value didn't match regex in input; "
+    RANGE_ERROR = "Value out of range; "
 
     return_string = ""
 
@@ -35,6 +38,7 @@ def check_state_of_fields(fields:list[str], headers:list[str], frequency_map:set
             if IGNORE_COLUMNS.__contains__(header):
                 continue
             # catch if its not defined
+            # TODO: Reformat error message
             if frequency_map[header][field] < REPETITION_TO_NORMALIZE:
                 if input_format[header]["type"] == "list":
                     domain = input_format[header]["list"]
@@ -46,10 +50,10 @@ def check_state_of_fields(fields:list[str], headers:list[str], frequency_map:set
                         if domain.count(field) != 0:
                             break
                     return_string += LISTS_ERROR
-                # if input_format[header]["type"] == "regex":
-                #     regex = input_format[header]["regex"]
-                #     if(regex.match(field)):
-                #         return_string += REGEX_ERROR
+                if input_format[header]["type"] == "regex":
+                    regex = input_format[header]["regex"]
+                    if(re.match(field, regex)):
+                        return_string += REGEX_ERROR
                 if input_format[header]["type"] == "range":
                     lower_bound = int(input_format[header]["lower_bound"])
                     upper_bound = int(input_format[header]["upper_bound"])
@@ -94,6 +98,10 @@ if __name__ == "__main__":
                 if not field in frequency_map[header]:
                     frequency_map[header][field] = 0
                 frequency_map[header][field] += 1
+
+
+
+
         with open(BROKEN_DATA_PATH, "w") as broken_data_file:
             broken_data_file.write("")
         with open(BROKEN_DATA_PATH, "a") as broken_data_file:
@@ -110,4 +118,3 @@ if __name__ == "__main__":
                 state_of_line = check_state_of_fields(fields,  headers, frequency_map)
                 if state_of_line != "Clear":
                     broken_data_file.write(line.strip() + "," + state_of_line + "\n")
-
